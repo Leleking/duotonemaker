1c45ceb8ad0e88294b13441c1066ff347630cd92
Simeon Nortey <simeonnortey@gmail.com>
2023-04-20 21:23:25 +0000

    updated env

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,10 @@ export default {
     "@nuxtjs/dotenv",
   ],
 
+  env: {
+    UNSPLASH_CLIENT_ID: process.env.UNSPLASH_CLIENT_ID,
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
